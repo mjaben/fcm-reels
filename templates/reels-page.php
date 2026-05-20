c116b1272f08ce725257371facf1626f7723ab91
mjaben <alexaben291@gmail.com>
2026-05-20 16:27:23 +0100

    Fix: Desktop & Tablet Layout

--- templates/reels-page.php
+++ templates/reels-page.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
 </head>
